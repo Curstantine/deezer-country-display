e7678392e61ba910caff360e6c774c00d5dc1d91
Curstantine <curstantine@proton.me>
2024-06-24 13:30:19 +0530

    fix: messed up positions possibly from out-of-bound index

--- deezer-country-display.user.js
+++ deezer-country-display.user.js
@@ -4,18 +4,22 @@
 // @version      2024-06-24
 // @description  Displays deezer track availability
 // @author       Curstantine
-// @match        https://www.deezer.com/en/album/*
+// @match        https://www.deezer.com/*/album/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=deezer.com
 // @grant        none
 // @run-at context-menu
 // ==/UserScript==
 
 /** @typedef {{ id: string; name: string; disc: number | null; track: number | null; }} NodeBoundTrack */
-/** @typedef {{ id: number; title: number; track_position: number | null; available_countries: string[] }} APITrack */
+/** @typedef {{ id: number; title: number; track_position: number | null; disc_number: number | null; available_countries: string[] }} APITrack */
 
 const DEEZER_API_TRACK_URL = "https://api.deezer.com/track";
 const SCRIPT_TRACK_REGEX = /(https:\/\/www\.deezer\.com\/.{2}\/track\/)(\d*)$/;
 const DISPLAY_TRACK_NAME_REGEX = /(\d{1,9999})\.\s(.*$)/;
+
+const MATCHER_TRACK_LIST_CONTAINER = `div[role="rowgroup"].ZOZXb`;
+const MATCHER_TRACK_CONTAINER = `div[role="row"].JR0qJ`;
+const MATCHER_DISK_CONTAINER = `div[role="row"].nqfmm`;
 
 async function sleep(ms = 1000) {
 	await new Promise((resolve) => setTimeout(resolve, ms));
@@ -31,21 +35,23 @@ async function fetch_track_data(id) {
 	return json;
 }
 
-(async function() {
-	"use strict";
+/**
+ * @param {number} node_length
+ * @returns
+ */
+function traverse_track_list_container(node_length) {
+	const track_list_container = document.querySelector(MATCHER_TRACK_LIST_CONTAINER);
+	const track_containers = track_list_container.querySelectorAll(MATCHER_TRACK_CONTAINER);
 
-	/** @type {HTMLMetaElement[]} */
-	const nodes = document.querySelectorAll(`meta[property="music:song"]`);
-
-	const track_list_container = document.querySelector(`div[role="rowgroup"].ZOZXb`);
-	const track_containers = track_list_container.querySelectorAll(`div[role="row"].JR0qJ`);
-
-	if (track_containers.length !== nodes.length) {
+	if (track_containers.length !== node_length) {
 		throw new Error("The amount of meta nodes do not match the amount of tracks available");
 	}
 
-	/** @type {Record<number, { label_node: Element, title_node: HTMLSpanElement }>} */
+	/** @type {Record<string, { label_node: Element, title_node: HTMLSpanElement }>} */
 	const dom_list = {};
+	let current_track_position = 1;
+	let current_disk_position = 1;
+
 	for (let i = 0; i < track_containers.length; i++) {
 		const node = track_containers[i];
 		const label_node = node.querySelector(`div[role="gridcell"] div.A0Vbi`);
@@ -56,19 +62,34 @@ async function fetch_track_data(id) {
 			throw new Error(`Title content is empty for {i+1} item`);
 		}
 
-		const [_, track_no_str, __] = DISPLAY_TRACK_NAME_REGEX.exec(title_node.textContent);
-		const track_no = Number.parseInt(track_no_str);
-		dom_list[track_no] = { label_node, title_node };
+		dom_list[`${current_disk_position}.${current_track_position}`] = { label_node, title_node };
+		current_track_position++;
+
+		if (node.nextElementSibling?.matches(MATCHER_DISK_CONTAINER)) {
+			current_disk_position++;
+			current_track_position = 1;
+		}
 	}
+
+	return dom_list;
+}
+
+(async function() {
+	"use strict";
+
+	/** @type {HTMLMetaElement[]} */
+	const nodes = document.querySelectorAll(`meta[property="music:song"]`);
+	const dom_list = traverse_track_list_container(nodes.length);
+	console.log(dom_list);
 
 	for (let i = 0; i < nodes.length; i++) {
 		const node = nodes[i];
 		const [_, __, id] = SCRIPT_TRACK_REGEX.exec(node.content);
 		const data = await fetch_track_data(id);
 
-		const matched = dom_list[data.track_position];
+		const matched = dom_list[`${data.disc_number ?? 1}.${data.track_position}`];
+		console.log({ matched, data, id });
 		if (matched === undefined) {
-			console.log(dom_list, matched, `${data.track_position}. ${data.title}`);
 			throw new Error(`Failed to a match track from the track list. Current: ${data}`);
 		}
 
